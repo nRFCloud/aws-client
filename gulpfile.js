52983798ca522578911096904bbd1cd0f7e3f733
Nathan Kerr <Nathan.Kerr@nordicsemi.no>
2018-05-22 14:06:33 -0700

    Better gulp tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,37 @@
 const gulp = require('gulp');
 const concat = require('gulp-concat');
+const uglify = require('gulp-uglify-es').default;
+const webpack = require('webpack');
+const gulpWebpack = require('webpack-stream');
 
-gulp.task('builddev', () => {
+gulp.task('compiledev', () => {
+	return gulp.src('./src/index.ts')
+		.pipe(gulpWebpack(require('./webpack.dev.js'), webpack))
+		.pipe(gulp.dest('dist/'));
+});
+
+gulp.task('builddev', ['compiledev'], () => {
+
 	return gulp.src(['./dist/aws-wrapper.js', './src/aws-iot-sdk-browser-bundle.min.js'])
 		.pipe(concat('aws-wrapper.js'))
 		.pipe(gulp.dest('./dist'));
 });
 
-gulp.task('default', () => {
+gulp.task('compileprod', () => {
+	return gulp.src('./src/index.ts')
+		.pipe(gulpWebpack(require('./webpack.prod.js'), webpack))
+		.pipe(gulp.dest('dist/'));
+});
+
+gulp.task('buildprod', ['compileprod'], () => {
 	return gulp.src(['./dist/aws-wrapper.min.js', './src/aws-iot-sdk-browser-bundle.min.js'])
 		.pipe(concat('aws-wrapper.min.js'))
+		.pipe(gulp.dest('./dist'));
+});
+
+gulp.task('default', ['buildprod'], () => {
+	return gulp.src('./dist/aws-wrapper.min.js')
+		.pipe(uglify())
 		.pipe(gulp.dest('./dist'));
 });
 
