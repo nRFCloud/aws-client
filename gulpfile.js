96750b2abccb33593035f4c14529f9fc6d50b97e
Cole Hafner <colehafner@gmail.com>
2019-02-08 12:08:41 -0800

    removed unused package.

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,10 @@
 const gulp = require('gulp');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify-es').default;
-const header = require('gulp-header');
 const banner = require('gulp-banner');
 const webpack = require('webpack');
 const gulpWebpack = require('webpack-stream');
 const { version } = require('./package.json');
-// console.log('VERSION', version);
-// process.exit();
 
 gulp.task('builddev', ['compiledev'], () => {
 	return gulp.src(['./dist/aws-wrapper.js', './src/aws-iot-sdk-browser-bundle.min.js'])
