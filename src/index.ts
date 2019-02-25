4b3d8988ec59457fe36d2bbe3fdc1c9773deb01c
Marko Schulz <Marko.Schulz@nordicsemi.no>
2019-02-25 20:23:02 +0100

    Break up long export line for readability

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
 export { default as AWSAppSyncClient } from 'aws-appsync';
 export { default as Cognito } from './Cognito';
 export { isDevZoneLogin } from './DevzoneHelper';
-export { ApolloConsumer, ApolloProvider, Query, Mutation, Subscription, graphql } from 'react-apollo';
+export {
+    ApolloConsumer,
+    ApolloProvider,
+    Mutation,
+    Query,
+    Subscription,
+    graphql,
+} from 'react-apollo';
 export { default as gql } from 'graphql-tag';
