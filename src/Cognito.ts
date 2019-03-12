49c83bb2b4133cc9548ba51c9fd2fa028394e1a8
Nathan Kerr <Nathan.Kerr@nordicsemi.no>
2019-03-12 06:29:59 -0700

    Remove react-apollo and graphql-tag

--- src/Cognito.ts
+++ src/Cognito.ts
@@ -5,8 +5,8 @@ import * as AmazonCognitoIdentity from 'amazon-cognito-identity-js';
 import { CognitoUser, CognitoUserPool, CognitoUserSession } from "amazon-cognito-identity-js";
 import { dzRefreshEndpoint, getCredentials, getQueryStringValue } from './DevzoneHelper';
 
-const {CognitoSyncManager, CognitoIdentityCredentials} = AWS; // provided by dist/amazon-cognito.min.js from https://github.com/aws/amazon-cognito-js (NOT Amplify!)
-const {AuthenticationDetails, CognitoUser, CognitoRefreshToken, CognitoUserAttribute, CognitoUserPool} = AmazonCognitoIdentity; // provided by dist/amazon-cognito-identity.min.js from https://github.com/aws/aws-amplify/tree/master/packages/amazon-cognito-identity-js
+const { CognitoSyncManager, CognitoIdentityCredentials } = AWS; // provided by dist/amazon-cognito.min.js from https://github.com/aws/amazon-cognito-js (NOT Amplify!)
+const { AuthenticationDetails, CognitoUser, CognitoRefreshToken, CognitoUserAttribute, CognitoUserPool } = AmazonCognitoIdentity; // provided by dist/amazon-cognito-identity.min.js from https://github.com/aws/aws-amplify/tree/master/packages/amazon-cognito-identity-js
 
 // TODO: Make COGNITO_USER_POOL_ID configurable externally
 // TODO: Make COGNITO_CLIENT_ID configurable externally
